33a87276f091c65a88f4b7a62c5e6b7363fdb7a9
Rafael Kollyfas <b7040474@newcastle.ac.uk>
2020-04-15 18:49:26 +0100

    location changes 2

--- app/src/main/java/com/example/ontime/Navigate.java
+++ app/src/main/java/com/example/ontime/Navigate.java
@@ -380,7 +380,21 @@ public class Navigate extends FragmentActivity implements OnMapReadyCallback, Lo
         }
         else
         {
-
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
         }
     }
 
