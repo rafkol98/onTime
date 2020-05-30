43a52f6f6cd91bdf26c03d323b0afb83d09cfe9c
Rafael Kollyfas <b7040474@newcastle.ac.uk>
2020-05-30 20:08:25 +0100

    bottom nav

--- app/src/main/java/com/example/ontime/MPage.java
+++ app/src/main/java/com/example/ontime/MPage.java
@@ -1,11 +1,15 @@
 package com.example.ontime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
@@ -21,46 +25,72 @@ public class MPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_page);
 
-
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tab0 = (TabItem) findViewById(R.id.tab0);
-        tab1 = (TabItem) findViewById(R.id.tab1);
-        tab2 = (TabItem) findViewById(R.id.tab2);
-        viewPager = findViewById(R.id.viewpager);
-
-        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setCurrentItem(1);
-        pagerAdapter.notifyDataSetChanged();
-
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                if (tab.getPosition() == 0) {
-                    pagerAdapter.notifyDataSetChanged();
-                } else if (tab.getPosition() == 1) {
-                    pagerAdapter.notifyDataSetChanged();
-                } else if (tab.getPosition() == 2) {
-                    pagerAdapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
+//        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//        tab0 = (TabItem) findViewById(R.id.tab0);
+//        tab1 = (TabItem) findViewById(R.id.tab1);
+//        tab2 = (TabItem) findViewById(R.id.tab2);
+//        viewPager = findViewById(R.id.viewpager);
+//
+//        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+//        viewPager.setAdapter(pagerAdapter);
+//        viewPager.setCurrentItem(1);
+//        pagerAdapter.notifyDataSetChanged();
+//
+//
+//        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//                if (tab.getPosition() == 0) {
+//                    pagerAdapter.notifyDataSetChanged();
+//                } else if (tab.getPosition() == 1) {
+//                    pagerAdapter.notifyDataSetChanged();
+//                } else if (tab.getPosition() == 2) {
+//                    pagerAdapter.notifyDataSetChanged();
+//                }
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//
+//    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch(item.getItemId()){
+                    case R.id.nav_map:
+                        selectedFragment = new Tab0();
+                        break;
+                    case R.id.nav_home:
+                        selectedFragment = new Tab1();
+                        break;
+
+                        case R.id.nav_search:
+                        selectedFragment = new Tab2();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                return true;
+            }
+        };
 
     public void onBackPressed(){
 
