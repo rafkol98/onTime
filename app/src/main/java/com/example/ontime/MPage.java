de77acddfeec3ca4ae60d67c0dc6fff478ce1092
Rafael Kollyfas <b7040474@newcastle.ac.uk>
2020-05-31 16:08:48 +0100

    Counter update

--- app/src/main/java/com/example/ontime/MPage.java
+++ app/src/main/java/com/example/ontime/MPage.java
@@ -3,15 +3,23 @@ package com.example.ontime;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MPage extends AppCompatActivity {
 
@@ -20,15 +28,47 @@ public class MPage extends AppCompatActivity {
     private TabItem tab0, tab1, tab2;
     public PageAdapter pagerAdapter;
 
+    Button btnCounter;
+
+    FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("/profiles");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_page);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        final BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Tab1()).commit();
+
+
+        final String uId = currentFirebaseUser.getUid();
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Long childrenL = dataSnapshot.child(uId).child("trips").getChildrenCount();
+                String counter = Long.toString(childrenL);
+                if (childrenL != 0){
+                    BadgeDrawable badgeDrawable = bottomNav.getBadge(R.id.nav_trips);
+                    if (badgeDrawable == null)
+                        bottomNav.getOrCreateBadge(R.id.nav_trips).setNumber(Integer.parseInt(counter));
+                    }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
 
     }
 //        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
@@ -85,7 +125,7 @@ public class MPage extends AppCompatActivity {
                         selectedFragment = new Tab1();
                         break;
 
-                        case R.id.nav_search:
+                        case R.id.nav_trips:
                         selectedFragment = new Tab2();
                         break;
                 }
