ecfa6613264a5f5b04b1d83d6b585512d71ce334
Rafael Kollyfas <rafaelkoll@Rafaels-MBP.fritz.box>
2020-06-27 22:22:44 +0300

    Finisheds

--- app/src/main/java/com/example/ontime/SignIn_UpClasses/SelectTempSpeed.java
+++ app/src/main/java/com/example/ontime/SignIn_UpClasses/SelectTempSpeed.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.ontime.MainClasses.MPage;
 import com.example.ontime.R;
 import com.example.ontime.SignIn_UpClasses.Countdown;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SelectTempSpeed extends AppCompatActivity {
 
     //Initialise variables.
     Button slowBtn, quiteSlowBtn, averageBtn, quiteFastBtn, fastBtn;
+
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("/profiles");
+
+    FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +36,108 @@ public class SelectTempSpeed extends AppCompatActivity {
         quiteFastBtn = findViewById(R.id.buttonQuiteFast);
         fastBtn = findViewById(R.id.buttonFast);
 
-        //Assign 3.5 as the user's average speed value in the firebase database.
+        //Assign 3.5 km/h as the user's average speed value in the firebase database.
         slowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                double speed = 3.5;
+                //Get uId of the user.
+                String uId = currentFirebaseUser.getUid();
 
+                //Get average speed reference.
+                DatabaseReference childReff = dbRef.child(uId).child("Average Speed");
+
+                childReff.setValue(speed);
+
+                //Go to MPage.
+                Intent intent = new Intent(SelectTempSpeed.this, MPage.class);
+                startActivity(intent);
+                finish();
 
 
             }
         });
-//
 
+        //Assign 4 km/h as the user's average speed value in the firebase database.
+        quiteSlowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double speed = 4;
+                //Get uId of the user.
+                String uId = currentFirebaseUser.getUid();
+
+                //Get average speed reference.
+                DatabaseReference childReff = dbRef.child(uId).child("Average Speed");
+
+                childReff.setValue(speed);
+
+                //Go to MPage.
+                Intent intent = new Intent(SelectTempSpeed.this, MPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Assign 4.5 km/h as the user's average speed value in the firebase database.
+        averageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double speed = 4.5;
+                //Get uId of the user.
+                String uId = currentFirebaseUser.getUid();
+
+                //Get average speed reference.
+                DatabaseReference childReff = dbRef.child(uId).child("Average Speed");
+
+                childReff.setValue(speed);
+
+                //Go to MPage.
+                Intent intent = new Intent(SelectTempSpeed.this, MPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Assign 4.80 km/h as the user's average speed value in the firebase database.
+        quiteFastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double speed = 4.80;
+                //Get uId of the user.
+                String uId = currentFirebaseUser.getUid();
+
+                //Get average speed reference.
+                DatabaseReference childReff = dbRef.child(uId).child("Average Speed");
+
+                childReff.setValue(speed);
+
+                //Go to MPage.
+                Intent intent = new Intent(SelectTempSpeed.this, MPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        //Assign 5.10 km/h as the user's average speed value in the firebase database.
+        fastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double speed = 5.10;
+                //Get uId of the user.
+                String uId = currentFirebaseUser.getUid();
+
+                //Get average speed reference.
+                DatabaseReference childReff = dbRef.child(uId).child("Average Speed");
+
+                childReff.setValue(speed);
+
+                //Go to MPage.
+                Intent intent = new Intent(SelectTempSpeed.this, MPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
