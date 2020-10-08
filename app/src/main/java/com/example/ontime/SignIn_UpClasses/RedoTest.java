85754070ca02615438a3a2f42713a5370b700a10
rafaelkoll <rafcall98@hotmail.com>
2020-10-08 12:38:00 +0100

    redo test done, shows current average speed of user

--- app/src/main/java/com/example/ontime/SignIn_UpClasses/RedoTest.java
+++ app/src/main/java/com/example/ontime/SignIn_UpClasses/RedoTest.java
@@ -1,23 +1,66 @@
 package com.example.ontime.SignIn_UpClasses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.ontime.MainClasses.Trip;
+import com.example.ontime.MainClasses.TripListAdapter;
 import com.example.ontime.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
 
 public class RedoTest extends AppCompatActivity {
 
     //Initialise variables.
     Button letsGoBtn, laterBtn;
+    TextView textSpeed;
+
+    //Get firebase user.
+    FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("/profiles");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redo_test);
+
+        //Get uId of the user from the firebase database.
+        final String uId = currentFirebaseUser.getUid();
+
+        DatabaseReference userRef = dbRef.child(uId);
+
+        textSpeed = findViewById(R.id.textSpeed);
+
+
+       userRef.child("Average Speed").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String avgSpeed = dataSnapshot.getValue().toString();
+
+                textSpeed.setText(avgSpeed+" km/h");
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //Assign Buttons.
         letsGoBtn = findViewById(R.id.outsideGoNotBtn);
